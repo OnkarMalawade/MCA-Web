f9895260b2261e34eef43e5be236707b2d0d86c4
OnkarMalawade <132775903+OnkarMalawade@users.noreply.github.com>
2023-09-04 13:02:45 +0530

    Add files via upload

---
+++ Code05/demo.js
@@ -0,0 +1,26 @@
+function f1() {
+    document.getElementById('p1').style.backgroundColor='Red';
+    document.getElementById('p1').style.color='Yellow';
+    document.getElementById('p1').style.fontSize='30px';
+}
+function add()
+{
+  const rs = num1 + num2;
+  window.alert(rs);
+}
+function sub()
+{
+    const rs = num1 - num2;
+    window.alert(rs);
+}
+function mult()
+{
+    const rs = num1 * num2;
+    window.alert(rs);
+}
+function div()
+{
+    const rs = num1 / num2;
+  window.alert(rs);
+  
+}
---
+++ Code05/l1.js
@@ -0,0 +1,6 @@
+const num1 = parseFloat(prompt('Enter the first number '));
+const num2 = parseFloat(prompt('Enter the second number '));
+document.writeln(num1);
+document.writeln("<br>");
+document.writeln(num2);
+document.writeln("<br>");
